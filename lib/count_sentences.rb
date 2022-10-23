b77ac875ce7e8002f6598a1c98b0628a8168070c
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-23 14:43:16 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self.end_with?(".")
+    false
   end
-
+  
   def question?
-
+   return false unless self.end_with?("?")
+   true
   end
-
+  
   def exclamation?
-
+ return true if self.end_with?("!")
+ false
   end
-
+  
   def count_sentences
-
+    self.split(/[.!?]/).count
   end
 end
